4da07a7237f2ea54d202bb63ccbe0a74f2c5b634
eesoymilk <qiyoudaoyi@gmail.com>
2024-01-01 22:44:06 +0800

    Add autoformating

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,45 @@
 return {
-    'nvimtools/none-ls.nvim',
-    event = 'VeryLazy',
-    opts = function()
-        local null_ls = require('null-ls')
-        return {
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettierd,
-                null_ls.builtins.formatting.eslint,
-            }
-        }
-    end,
-    config = function(_, opts)
-        require('null-ls').setup(opts)
-    end,
+	"nvimtools/none-ls.nvim",
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				vim.lsp.buf.format({ async = false })
+			end,
+			desc = "Format",
+		},
+	},
+	opts = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		return {
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.eslint,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								async = false,
+								bufnr = bufnr,
+							})
+						end,
+					})
+				end
+			end,
+		}
+	end,
+	config = function(_, opts)
+		require("null-ls").setup(opts)
+	end,
 }
