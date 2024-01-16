60d26d35c260c5b178f045f1eb092f87afb8c1b4
qiyoudaoyi <qiyoudaoyi@gmail.com>
2024-01-16 21:53:07 +0800

    idk what i change, sorry

--- lua/soymilk/remap.lua
+++ lua/soymilk/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line(s) down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line(s) up" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -11,11 +11,13 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remaps ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste over selection" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("v", ">", ">gv", { desc = "Indent selection" })
+vim.keymap.set("v", "<", "<gv", { desc = "Unindent selection" })
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Open tmux sessionizer" })
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -26,6 +28,11 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
+vim.keymap.set("n", "<leader><leader>", vim.cmd.so, { desc = "Source file" })
 
-vim.keymap.set("n", "g?", "<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>")
+vim.keymap.set(
+	"n",
+	"g?",
+	"<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>",
+	{ desc = "Open line diagnostics" }
+)
